3673006c7aa0d4c9487a9d838a8ee9d428f34322
r4xjs <r4xjs@lowsec.net>
2021-06-16 20:05:40 +0200

    add rips-42 solution

--- rips-42/solution/challenge.php
+++ rips-42/solution/challenge.php
@@ -1,5 +1,7 @@
 $sanitized = [];
 
+// 1) The key and value of sanitized is user input
+//    The value must be an int but the key has no such limitation
 foreach ($_GET as $key => $value) {
     $sanitized[$key] = intval($value);
 }
@@ -10,5 +12,11 @@ $queryParts = array_map(function ($key, $value) {
 
 $query = implode('&', $queryParts);
 
+// 2) The key can be used for xss
 echo "<a href='/images/sized.php?" .
     htmlentities($query) . "'>link</a>";
+
+// example:
+// key = "aaa' onclick='alert(1)' x='"
+// value = 1
+// --> <a href='/images/sized.php?aaa' onclick='alert(1)' x='=5'>link</a>
